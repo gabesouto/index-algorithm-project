9379a033338624ba42e38960d73db7867e393edf
gabesouto <soutogabriel04@gmail.com>
2023-08-28 17:52:44 -0300

    Requisito 03/04/05

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,38 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    lines = txt_importer(path_file)
+    is_not_in_file = True
 
+    for file in instance.qeue :
+        if path_file == file["nome_do_arquivo"]:
+                is_not_in_file  = False
+
+    dict = {
+         "nome_do_arquivo": path_file,
+         "qtd_linhas": len(lines),
+         "linhas_do_arquivo": lines,
+         }
+
+    if is_not_in_file :
+        instance.enqueue(dict)
+    
+    print(dict)
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        print("Não há elementos", file=sys.stdout)
+        return
+
+    removed_file = instance.dequeue()
+    print(f"Arquivo {removed_file['nome_do_arquivo']} removido com sucesso", file=sys.stdout)
 
 
-def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+def file_metadata(instance, index):
+    if 0 <= index < len(instance):
+        metadata = instance.search(index)
+        print(metadata)
+    else:
+        print("Posição inválida", file=sys.stderr)
