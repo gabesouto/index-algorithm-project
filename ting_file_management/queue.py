efa200eff8ddf8bdf0faa528ed73fa9646951871
gabesouto <soutogabriel04@gmail.com>
2023-08-28 16:08:06 -0300

    Gabriel Souto  Project T.I.N.G

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,18 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.qeue = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.qeue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.qeue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.qeue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index >= len(self.qeue):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self.qeue[index]
