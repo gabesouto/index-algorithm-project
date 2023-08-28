04887ecd4086b7281c772c558dd5229c5aaf225d
gabesouto <soutogabriel04@gmail.com>
2023-08-28 17:08:31 -0300

    Requisito 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return None
+
+    try:
+        with open(path_file, "r") as file:
+            lines = file.read().splitlines()
+        return lines
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return None
