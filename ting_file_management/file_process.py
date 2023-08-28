4c79302016dbeab5134271fc3f61e8f14b84d776
gabesouto <soutogabriel04@gmail.com>
2023-08-28 18:06:42 -0300

    Flake fixed

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,25 +1,27 @@
 from ting_file_management.file_management import txt_importer
 import sys
 
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
     lines = txt_importer(path_file)
     is_not_in_file = True
 
-    for file in instance.qeue :
+    for file in instance.qeue:
         if path_file == file["nome_do_arquivo"]:
-                is_not_in_file  = False
+            is_not_in_file = False
 
-    dict = {
-         "nome_do_arquivo": path_file,
-         "qtd_linhas": len(lines),
-         "linhas_do_arquivo": lines,
-         }
+    file_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
 
-    if is_not_in_file :
-        instance.enqueue(dict)
-    
-    print(dict)
+    if is_not_in_file:
+        instance.enqueue(file_dict)
+
+    print(file_dict)
+
 
 def remove(instance):
     if len(instance) == 0:
@@ -27,7 +29,8 @@ def remove(instance):
         return
 
     removed_file = instance.dequeue()
-    print(f"Arquivo {removed_file['nome_do_arquivo']} removido com sucesso", file=sys.stdout)
+    print(f"Arquivo {removed_file['nome_do_arquivo']} removido com sucesso",
+          file=sys.stdout)
 
 
 def file_metadata(instance, index):
